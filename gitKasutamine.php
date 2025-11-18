289f061efbcb1c3c9f37bd3fd5113e15a8ca9cd1
Artjom Põldsaar <artjompoldsaar@gmail.com>
2025-11-18 09:35:05 +0200

    git konspekt loodud

--- gitKasutamine.php
+++ gitKasutamine.php
@@ -29,6 +29,20 @@ id_rsa.pub võti kooperitakse GitHub'i nagu Deploy Key
 </pre>
 
 <?php
-echo "<li>GitHub projektiga sidumine</li>"
+echo "<li>GitHub projektiga sidumine</li>";
+echo "<pre>";
+echo "git remote add origin git@github.com:GummyisHear/phpTund.git
+git branch -M main
+git push -u origin main";
+echo "</pre>";
+
+echo "<li>Projekti kloonimine desktopi.<br>
+ * Kontrolli, et id_rsa võti on olemas .ssh kaustas.<br>
+ * GIT CMD on lahti ja<br>
+<pre>
+    git clone git@github.com:GummyisHear/phpTund.git
+</pre>
+</li>";
+echo "</ol>";
 
 ?>
